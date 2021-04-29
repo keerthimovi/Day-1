9d4133abc93a7e348398779ff6c45984847c4482
Keerthana nandagopal <83340271+keerthimovi@users.noreply.github.com>
2021-04-29 04:50:48 -0700

    Add files via upload

---
+++ Day 2/selenium test.cs
@@ -0,0 +1,36 @@
+﻿
+
+using OpenQA.Selenium;  
+using OpenQA.Selenium.Chrome;  
+using System;  
+ 
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Automation test started ");
+            IWebDriver driver = new ChromeDriver(@"F:\driver c sharp");
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        
+            IWebElement ele = driver.FindElement(By.Name("q"));
+            ele.SendKeys("KLA Tencor");
+            
+            IWebElement ele1 = driver.FindElement(By.Name("btnK"));
+            ele1.Click();
+            
+
+            driver.Close();
+            Console.Write("The Test case is ended ");
+        }
+    }
+}
+
+
+
+
+
+
+
