0c71d445747b231424437a8298b5ddb18db09b19
Keerthana nandagopal <83340271+keerthimovi@users.noreply.github.com>
2021-04-28 23:30:25 -0700

    occurences of words

---
+++ Day 2/occurences.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+class occurences
+{
+static void Main()
+    {
+        string str = "Do you see what i see which other cant see";
+        Console.WriteLine("Occurrence:" + Check.CheckOccurrences(str, "see"));
+    }
+}
+public static class Check
+{
+ public static int CheckOccurrences(string sentences, string style)
+    {
+        int count = 0;
+        int a = 0;
+        while ((a = sentences.IndexOf(style, a)) != -1)
+        {
+            a += style.Length;
+            count++;
+        }
+        return count;
+    }
+}
